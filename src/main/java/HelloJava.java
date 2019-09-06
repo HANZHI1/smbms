051570bb3844753b3ea702d792e95fe01015cac3
dell <744862674@qq.com>
2019-09-06 13:53:12 +0800

    饥籍家家米

---
+++ src/main/java/HelloJava.java
@@ -0,0 +1,18 @@
+public class HelloJava {
+    //main方法快捷键：psvm
+    //foreach快捷键:iter
+    //get/set方法快捷键:alt+insert
+    //代码块快捷键:ctrl+alt+t
+    //快速修复:ctrl+alt+v
+    //快速构造方法:alt+enter
+    //代码向上下移动:ctrl+shift+上/下
+    //复制:ctrl+d
+    //删除:ctrl+y
+    //输出语句:sout
+
+    public static void main(String[] args) {
+       
+    }
+
+
+}
---
+++ src/main/java/com/githup/mybatis/dao/UserMapper.java
@@ -0,0 +1,45 @@
+package com.githup.mybatis.dao;
+
+import com.githup.mybatis.pojo.User;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.HashMap;
+import java.util.List;
+
+public interface UserMapper {
+    /**
+     * 查询所有用户
+     * @return
+     */
+    List<User> findUser();
+
+    /**
+     * 根据用户姓名进行模糊查询
+     * @param name
+     * @return
+     */
+    User findUserByUserName(String name);
+
+    /**
+     * 根据角色id用户名进行查询用户
+     * @param user
+     * @return
+     */
+    User findUserByUserNameAndUserRole(User user);
+
+    /**
+     * 根据map集合来查询用户信息
+     * @param map
+     * @return
+     */
+    User findUserByMap(HashMap<String, Object> map);
+
+    /**
+     * 注解param就可以将参数注入到UserMapper.xml文件中
+     * 根据姓名和角色id来查询用户信息
+     * @param name
+     * @param role
+     * @return
+     */
+    User findUserByUserNameAndUserRole1(@Param("userName") String name ,@Param ("userRole")int role);
+}
